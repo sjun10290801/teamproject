f9965b7e2c825733bc0d24cca944750987708b53
woo0303 <wjddntjd200@gmail.com>
2026-07-30 00:30:55 +0900

    상세보기

--- main_top.php
+++ main_top.php
@@ -54,7 +54,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <nav class="navbar bg-body-tertiary fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">중고거래</a>
+    <a class="navbar-brand" href="index.php">중고거래</a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
