9fcd5aa7621a52420deca728a9df0aa1d40d612f
sjun10290801 <sjun10290801@gmail.com>
2026-07-29 22:21:02 +0900

    링크 수정 및 페이지네이션 함수 추가

--- main_top.php
+++ main_top.php
@@ -54,7 +54,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <nav class="navbar bg-body-tertiary fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">중고거래</a>
+    <a class="navbar-brand" href="index.php">중고거래</a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -70,19 +70,19 @@
             <a class="nav-link active" aria-current="page" href="login.php">로그인</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">채팅</a>
+            <a class="nav-link" href="chat_list.php">채팅</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="member_mypage.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               마이페이지
             </a>
           </button>
           <ul class="dropdown-menu dropdown-menu-lights">
             <li><a class="dropdown-item" href="member_mypage.php">내정보</a></li>
             <li><a class="dropdown-item" href="#">찜한목록</a></li>
-            <li><a class="dropdown-item" href="#">상품등록</a></li>
-            <li><a class="dropdown-item" href="#">판매관리</a></li>
-            <li><a class="dropdown-item" href="#">구매내역</a></li>
+            <li><a class="dropdown-item" href="product_create.php">상품등록</a></li>
+            <li><a class="dropdown-item" href="member_mypage.php?kind=sell">판매관리</a></li>
+            <li><a class="dropdown-item" href="member_mypage.php?kind=buy">구매내역</a></li>
           </ul>
           </li>
         </ul>
