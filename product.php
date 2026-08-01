cce1891db78043c7e665820b08b2b70d3f0c220f
sjun10290801 <sjun10290801@gmail.com>
2026-08-01 16:52:03 +0900

    상품 찜목록 링크 오류 수정

--- product.php
+++ product.php
@@ -53,7 +53,7 @@
     </ul>
     <div class="card-body">
                 <div class="position-relative" style="z-index: 2;">
-                <a href="good_insert.php?product_id=$product_id" class="card-link text-decoration-none">찜하기</a>
+                <a href="good_insert.php?product_id=<?php echo $product_id; ?>" class="card-link text-decoration-none">찜하기</a>
                 <a href="chat_room.php?my_id=<?php echo $member_id;?>&target_id=<?php echo $row["member_id"];?>&product_id=<?php echo $product_id;?>"
                  class="card-link text-decoration-none">채팅하기</a>
                 <a href="member_profile.php?id=<?php echo $row["member_id"];?>" class="card-link text-decoration-none">프로필보기</a>
