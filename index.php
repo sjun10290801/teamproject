c9e013eb2ba7f0328fa5feee37ef14a50a3d42c0
woo0303 <wjddntjd200@gmail.com>
2026-08-04 13:45:18 +0900

    배너

--- index.php
+++ index.php
@@ -6,25 +6,7 @@
 ?>
 
 
-
-
-
-
-<ul class="nav align-items-center">
-  <li class="nav-item">
-    <span class="nav-link fw-bold text-dark ps-0">카테고리</span>
-  </li>
-<?php
-    for($i = 1; $i < $n_category; $i++) {
-?>
-  <li class="nav-item">
-    <a class="nav-link text-secondary" href="category.php?menu=<?php echo $i ?>"><?php echo $a_category[$i]; ?></a>
-  </li>
-<?php } ?>
-  
-</ul>
-
-<div id="carouselExampleIndicators" class="carousel slide">
+<div id="carouselExampleIndicators" class="carousel slide mt-3 mb-2">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -50,6 +32,23 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+
+
+
+<ul class="nav align-items-center">
+  <li class="nav-item">
+    <span class="nav-link fw-bold text-dark ps-0">카테고리</span>
+  </li>
+<?php
+    for($i = 1; $i < $n_category; $i++) {
+?>
+  <li class="nav-item">
+    <a class="nav-link text-secondary" href="category.php?menu=<?php echo $i ?>"><?php echo $a_category[$i]; ?></a>
+  </li>
+<?php } ?>
+  
+</ul>
+
 
 <div class="d-flex justify-content-end px-4 mt-3 mb-2">
       <div class="dropdown">
