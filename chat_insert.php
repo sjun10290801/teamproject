c4650583bc9fcc1cdcce9ac63234ebe2de2df171
sjun10290801 <sjun10290801@gmail.com>
2026-08-14 14:46:55 +0900

    채팅창 테이터 검증 및 트랜잭션 적용

--- chat_insert.php
+++ chat_insert.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
     include "common.php";
 
     // post 방식으로 데이터 불러오기
@@ -11,43 +9,36 @@ ini_set('display_errors', 1);
 
     // 이미지 삽입은 상품의 이미지 삽입과 동일
     $filename = $_FILES["image"]["name"]; // 이미지 이름
-    if($filename) {
-        $tmp = strtolower(pathinfo($filename, PATHINFO_EXTENSION)); // strtolower => 영어 소문자로 변경하는 함수
-        // pathinfo(경로, PATHINFO_EXTENSION) => 파일의 확장자만 추출하기 위한 함수
 
-        switch($tmp) { // 확장자가 이미지가 아니면 종료
-            case "png": case "jpg": case "jpeg":
-                break;
-            default:
-                echo("이미지(png, jpg, jpeg) 파일만 업로드 가능합니다.");
-                exit();
-        }
-    
-
-    
-        // 파일 이름 중복 방지 -> "chat" + 제품id
-        $sql = "select chat_id from chat order by chat_id desc"; // 제품 id 내림차순 정렬
-        $result = mysqli_query($db, $sql);
-        if(!$result) exit("에러 : $sql");
-
-        if($row = mysqli_fetch_assoc($result)) { // 제품이 없다면 제품 id는 1. 제품이 있다면 마지막 제품id + 1
-            $chat_id = $row["chat_id"] + 1;
-        } else {
-            $chat_id = 1;
-        }
-
-        $fname = "chat".$chat_id.".".$tmp;
-        if($_FILES["image"]["error"] == 0)
-        {
-            if(!move_uploaded_file($_FILES["image"]["tmp_name"],"chat/$fname")) // 업로드
-                exit("업로드 실패");
-        }
+    if(!$text && !$filename) {
+        echo("<script>alert('내용을 입력해주세요');</script>");
+        echo("<script>window.history.back();</script>");
+        exit();
     }
 
+    mysqli_begin_transaction($db); // 트랜잭션 시작
+
+    // 이미지 업로드
+    $fname = imageUpload("chat", "chat", "chat");
+    if(!$fname) {
+        mysqli_rollback($db);
+        echo("<script>alert('오류가 발생했습니다');</script>");
+        echo("<script>window.history.back();</script>");
+        exit();
+    }
+
+    // 채팅 데이터 저장
     $sql = "insert into chat(to_member_id, from_member_id, product_id, text, reg_date, state, image)
             values($to_member_id, $from_member_id, $product_id, '$text', sysdate(), 0, '$fname')";
     $result = mysqli_query($db, $sql);
-    if(!$result) exit("에러 : $sql");
+    if(!$result) {
+        mysqli_rollback($db);
+        echo("<script>alert('오류가 발생했습니다');</script>");
+        echo("<script>window.history.back();</script>");
+        exit();
+    }
+
+    mysqli_commit($db);
 
     header("Location:chat_room.php?my_id=$from_member_id&target_id=$to_member_id&product_id=$product_id");
 ?>
