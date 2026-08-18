7d663d89d563dc2c46977b802c0899e4c4c71898
woo0303 <wjddntjd200@gmail.com>
2026-08-18 14:13:17 +0900

    bottom 수정

--- main_bottom.php
+++ main_bottom.php
@@ -1,15 +1,77 @@
-<div class="container d-flex justify-content-end">
-  <dl>
-    <dt>회사명</dt>
-    <dd>인덕대 중고거래 사이트</dd>
-    <dt>대표명</dt>
-    <dd>허유정</dd>
-    <dt>전화번호</dt>
-    <dd>010-1111-1111</dd>
-    <dt>위치</dt>
-    <dd>서울특별시 노원구</dd>
-  </dl>
-</div>
-</body>
+<div class="container">
 
+  <footer class="py-5 border-top"
+          style="border-color: #18766d !important;">
+
+    <div class="row">
+
+
+      <!-- 사이트 소개 -->
+      <div class="col-md-6 mb-3">
+
+        <h5 class="fw-bold" style="color: #18766d;">
+          리픽
+        </h5>
+
+        <p class="text-body-secondary mb-0">
+          인덕대학교 중고거래 사이트
+        </p>
+
+        <p class="text-body-secondary mt-2">
+          필요한 물건을 사고 팔 수 있는<br>
+          편리한 중고거래 공간입니다.
+        </p>
+
+      </div>
+
+
+      <!-- 회사정보 -->
+      <div class="col-md-4 mb-3">
+
+        <h5 class="fw-bold" style="color: #18766d;">
+          회사정보
+        </h5>
+
+        <ul class="nav flex-column">
+
+          <li class="nav-item mb-2">
+            <span class="text-body-secondary">
+              회사명 : 인덕대학교 중고거래 사이트
+            </span>
+          </li>
+
+          <li class="nav-item mb-2">
+            <span class="text-body-secondary">
+              대표명 : 허유정
+            </span>
+          </li>
+
+          <li class="nav-item mb-2">
+            <span class="text-body-secondary">
+              전화번호 : 010-1111-1111
+            </span>
+          </li>
+
+          <li class="nav-item mb-2">
+            <span class="text-body-secondary">
+              위치 : 서울특별시 노원구
+            </span>
+          </li>
+
+        </ul>
+
+      </div>
+
+    </div>
+
+
+    <!-- 하단 구분선 -->
+    <div class="border-top mt-4 pt-3">
+    </div>
+
+  </footer>
+
+</div>
+
+</body>
 </html>
