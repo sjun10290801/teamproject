16d8020f6bb20e2d82d051ec4e041ae56efa059c
moongchitory <132593206+moongchitory@users.noreply.github.com>
2026-08-23 15:32:40 +0900

    마이페이지 판매 상품 화면 수정 및 폰트 오류 판매내역 수정버튼 삭제

--- main_top.php
+++ main_top.php
@@ -12,7 +12,7 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 
-  <style>z
+  <style>
     body {
       font-family: "NanumSquareRound", sans-serif;
       padding-top: 50px;
