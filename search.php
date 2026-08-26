7b7e9b357305410578c7538647d164ceb9347cba
woo0303 <wjddntjd200@gmail.com>
2026-08-26 16:19:48 +0900

    검색 더보기 수정

--- search.php
+++ search.php
@@ -94,6 +94,7 @@ if(!$result) exit("에러 : $sql");
   echo ("<script>order_scroll();</script>");
 ?>
 
+<main class="py-4">
 
 <div class="my-4 px-2">
 
@@ -361,6 +362,18 @@ if(!$result) exit("에러 : $sql");
     box-shadow: 0 0 0 0.2rem rgba(24, 118, 109, 0.15);
 }
 
+.main-btn {
+    background-color: #18766d;
+    border-color: #18766d;
+    color: white;
+}
+
+.main-btn:hover {
+    background-color: #105f58;
+    border-color: #105f58;
+    color: white;
+}
+
 </style>
 
 
@@ -568,8 +581,15 @@ if(!$result) exit("에러 : $sql");
 </div>
 
 
-<?php echo $pagebar; ?>
+<div class="text-center mt-5">
+    <a href="category.php?menu=<?php echo $category; ?>"
+       class="btn main-btn rounded-pill px-5 py-2 fw-semibold">
+        상품 더보기
+        <i class="bi bi-chevron-right ms-1"></i>
+    </a>
+</div>
 
+</main>
 
 <?php
 
